b5b49a8d509ebeda968ad50b0f59767d387523c2
atere21 <you@tosinateret@gmail.com>
2023-11-23 05:45:00 -0800

    edited

--- src/components/FAQ's/Faq.jsx
+++ src/components/FAQ's/Faq.jsx
@@ -1,31 +1,26 @@
-import React from 'react'
-import { data } from "../../components/data/data.js"
-import { Accordion } from './Accordion'
+import React from 'react';
+import { data } from "../../components/data/data.js";
+import { Accordion } from './Accordion';
 import useOpenController from './UseOpenController';
 import { useEffect } from 'react';
 
-export const Faq= () => {
-
-   
+export const Faq = () => {
   return (
-    <div
-   
-     className='w-full h-full px-4 bg-white text-[#030355] m-8 p-8 grid place-items-center
-      max-w-[1240px] mx-auto md:mt-[4rem]  Faq'>
-        <div className='px-[2px] w-full '>
-        <h3 className='my-4 justify-center font-abc text-2xl md:text-4xl flex  items-center'>
-        INNER CIRCLE TRADING CLUB FREQUENTLY ASKED QUESTIONS (FAQs)
-       </h3>
-       <div className=''>
-        {data.map((section,index)=> (
-            <Accordion key={index} section={section}/>
-        )
-        )}
-
-       </div>
-       </div>
+    <div className='w-full h-full px-4 bg-white text-[#030355] m-8 p-8 grid place-items-center max-w-[1240px] mx-auto md:mt-[4rem] Faq'>
+      <div className='px-[2px] w-full'>
+        <h3 className='my-4 justify-center font-abc text-2xl md:text-4xl mb-8 flex items-center'>
+          INNER CIRCLE TRADING CLUB FREQUENTLY ASKED QUESTIONS (FAQs)
+        </h3>
+        <div className=''>
+          {data.map((section, index) => (
+            <div key={index} className='border-b-2 border-blue-500 py-4'>
+              <Accordion section={section} />
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Faq
+export default Faq;
--- src/components/Offerings.jsx
+++ src/components/Offerings.jsx
@@ -3,32 +3,86 @@ import React from 'react';
 const Offerings = () => {
   return (
     <div className='w-screen h-full py-10 bg-gray-300 text-[#030355]'>
-      <div className='max-w-full max-h-full mx-auto px-20'>
-        <h1 className='text-3xl grid sm:text-6xl text-center mb-8'>Our Offerings</h1>
-        <div className='grid grid-cols-1 sm:grid-cols-3 gap-8'>
+      <div className='max-w-full max-h-[70%] mx-auto px-20'>
+        <h1 className='text-3xl grid sm:text-6xl text-center mb-8 font-bold'>Our Offerings</h1>
+        <div className='grid grid-cols-1 sm:grid-cols-4 gap-8  justify-center items-center'>
           {/* Box 1 */}
-          <div className='group bg-white rounded-lg shadow-md transform transition duration-500 ease-in-out hover:rotate-y-180 hover:bg-blue-500 overflow-hidden h-[500px]'>
-            <div className='w-full h-full shadow-xl rounded-lg p-4 hover:scale-105 duration-300'>
-              <h2 className='group-hover:hidden text-2xl md:text-4xl'>Front Text 1</h2>
-              <p className='hidden group-hover:flex text-lg md:text-xl'>Back Text 1</p>
+          <div className='group bg-gray-500 rounded-lg shadow-md overflow-hidden'>
+            <div className='w-full h-[300px] group-hover:bg-[#030355] perspective-card'>
+              <div className='card-content p-4 h-full flex-col justify-center items-center'>
+                <div className='text-2xl md:text-4xl flex flex-col justify-center items-center mt-14'>
+                  <h2 className='group-hover:hidden mb-10'>Live Trades</h2>
+                  <div className='border-2 border-yellow-400 group-hover:hidden w-20'></div>
+                  <h2 className='group-hover:hidden text-2xl md:text-4xl mt-6'>Access</h2>
+                </div>
+                <div className='max-w-4xl'>
+                  <p className='hidden group-hover:flex text-lg p-4 mt-6 md:text-2xl  text-white justify-center items-center'>
+                    ICTC members have instant access to view ongoing trades through investor password.
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
+          
  
-          {/* Box 2 */}
-          <div className='group bg-white rounded-lg shadow-md transform transition duration-500 ease-in-out hover:rotate-y-180 hover:bg-blue-500 overflow-hidden h-[500px]'>
-            <div className='w-full shadow-xl rounded-lg p-4 hover:scale-105 duration-300'>
-              <h2 className='group-hover:hidden text-2xl md:text-4xl'>Front Text 2</h2>
-              <p className='hidden group-hover:flex text-lg md:text-xl'>Back Text 2</p>
+         {/* Box 1 */}
+          <div className='group bg-gray-500 rounded-lg shadow-md overflow-hidden'>
+            <div className='w-full h-[300px] group-hover:bg-[#030355] perspective-card'>
+              <div className='card-content p-4 h-full flex-col justify-center items-center'>
+                <div className='text-2xl md:text-4xl flex flex-col justify-center items-center mt-14'>
+                  <h2 className='group-hover:hidden mb-10'>AI-Powered</h2>
+                  <div className='border-2 border-yellow-400 group-hover:hidden w-20'></div>
+                  <h2 className='group-hover:hidden text-2xl md:text-4xl mt-2'>Risk Management</h2>
+                </div>
+                <div className='max-w-4xl'>
+                  <p className='hidden group-hover:flex text-lg p-4 mt-0 md:text-2xl 
+                   text-white justify-center items-center'>
+                  ICTC uses AI-powered risk management tools that monitor and analyze trading behavior, providing personalized risk alerts and suggestions.
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
+          
+ {/* Box 1 */}
+ <div className='group bg-gray-500 rounded-lg shadow-md overflow-hidden'>
+            <div className='w-full h-[300px] group-hover:bg-[#030355] perspective-card'>
+              <div className='card-content p-4 h-full flex-col justify-center items-center'>
+                <div className='text-2xl md:text-4xl flex flex-col justify-center items-center mt-14'>
+                  <h2 className='group-hover:hidden mb-10 justify-center items-center'>Transparency & <br/> Security</h2>
+                  <div className='border-2 border-yellow-400 group-hover:hidden w-20'></div>
+                  <h2 className='group-hover:hidden text-2xl md:text-4xl mt-4'>Operations</h2>
+                </div>
+                <div className='max-w-4xl'>
+                  <p className='hidden group-hover:flex text-lg p-4 mt-2 md:text-2xl  text-white justify-center items-center'>
+                  ICTC ensures transparency, and security, and safeguard your capital, allowing confident trading without fear.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          
+ 
 
-          {/* Box 3 */}
-          <div className='group bg-white rounded-lg shadow-md transform transition duration-500 ease-in-out hover:rotate-y-180 hover:bg-blue-500 overflow-hidden h-[500px]'>
-            <div className='w-full shadow-xl rounded-lg p-4 hover:scale-105 duration-300'>
-              <h2 className='group-hover:hidden text-2xl md:text-4xl'>Front Text 3</h2>
-              <p className='hidden group-hover:flex text-lg md:text-xl'>Back Text 3</p>
+           {/* Box 1 */}
+           <div className='group bg-gray-500 rounded-lg shadow-md overflow-hidden'>
+            <div className='w-full h-[300px] group-hover:bg-[#030355] perspective-card'>
+              <div className='card-content p-4 h-full flex-col justify-center items-center'>
+                <div className='text-2xl md:text-4xl flex flex-col justify-center items-center mt-14'>
+                  <h2 className='group-hover:hidden mb-10'>Educational</h2>
+                  <div className='border-2 border-yellow-400 group-hover:hidden w-20'></div>
+                  <h2 className='group-hover:hidden text-2xl md:text-4xl mt-6'>Webinar</h2>
+                </div>
+                <div className='max-w-4xl'>
+                  <p className='hidden group-hover:flex text-lg p-4 mt-0 md:text-2xl  text-white justify-center items-center'>
+                  Learn from ICTC experienced traders and experts through webinars covering our strategies, financial literacy, market trends and insights.
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
+          
+ 
         </div>
       </div>
     </div>
